254c45207fe7d2235fcd010120889f75b5852619
losrobbos <losrobbos@web.de>
2021-08-19 16:49:10 +0200

    Added solution

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+// SETUP BODY PARSING
+app.use( express.json() ) // => applied to BEFORE EVERY request
+
 let fruits = [
   { id: "1", name: 'Banana', color: 'yellow'},
   { id: "2", name: 'Cherry', color: 'red'},
@@ -14,7 +17,25 @@ app.get('/fruits', (req, res) => {
 });
 
 app.get('/fruits/:id', (req, res) => {
-  res.json({})
+  let fruit = fruits.find(fruit => fruit.id == req.params.id)
+  res.json(fruit)
+})
+
+app.post('/fruits', (req, res) => {
+  let fruitNew = {...req.body}
+  fruits.push(fruitNew)
+  res.json(fruit)
+})
+
+app.patch('/fruits/:id', (req, res) => {
+  let fruitToUpdate = fruits.find(fruit => fruit.id == req.params.id)
+  Object.assign(fruitToUpdate, {...req.body})
+  res.json(fruitToUpdate)
+})
+
+app.delete('/fruits/:id', (req, res) => {
+  fruits = fruits.filter(fruit => fruit.id !== req.params.id)
+  res.json(fruits)
 })
 
 app.listen(5000, () => {
